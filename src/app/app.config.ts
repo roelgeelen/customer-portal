bd0639c2827485fd9158785c3fded30aba0dc4c8
R. Geelen <roel-geelen@hotmail.com>
2026-04-10 13:44:33 +0200

    first claude localization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,19 @@
 import {ApplicationConfig} from '@angular/core';
 import {provideRouter, withComponentInputBinding} from '@angular/router';
+import {registerLocaleData} from '@angular/common';
+import localeNl from '@angular/common/locales/nl';
+import localeEn from '@angular/common/locales/en';
 
 import {routes} from './app.routes';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {SignatureParamInterceptor} from "./_helpers/interceptors/http.interceptor";
 import {AuthInterceptor} from "./_helpers/interceptors/auth.interceptor";
+
+// Register locale data so Angular's date/number/currency pipes work correctly
+// for both locales. LOCALE_ID itself is set automatically by the i18n build.
+registerLocaleData(localeNl);
+registerLocaleData(localeEn);
 
 export const appConfig: ApplicationConfig = {
   providers: [
